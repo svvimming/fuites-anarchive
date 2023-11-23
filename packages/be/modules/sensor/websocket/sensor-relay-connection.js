bcd28fd472b1e61109203c149d117939b67a4543
svvimming <benmunozj@gmail.com>
2023-11-23 15:48:18 -0500

    feat: sensor data relay

--- packages/be/modules/sensor/websocket/sensor-relay-connection.js
+++ packages/be/modules/sensor/websocket/sensor-relay-connection.js
@@ -7,8 +7,19 @@ const MC = require('@Root/config')
 // //////////////////////////////////////////////////////////////////// Endpoint
 // -----------------------------------------------------------------------------
 MC.socket.listeners.push({
-  name: 'interweave|ping',
-  async handler (data) {
-    console.log('ping received', data)
+  name: 'interweave|sensor-data',
+  async handler (sensor, data) {
+    console.log('ping received', sensor, data)
+    data.$inc = { update_count: 1 }
+    const updated = await MC.mongoInstances.interweave.model.Thingie
+      .updateMany({ sensors_active: { $in: [sensor] } }, data, { new: true })
+      .populate({
+        path: 'file_ref',
+        select: 'filename file_ext aspect'
+      })
+    MC.socket.io
+      .of('/interweave')
+      .to('interweave|thingies')
+      .emit('module|sensor-data-update|payload', updated)
   }
 })
