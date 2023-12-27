0d392656c8c511e5ac21696a1cb668cd5c78cd03
svvimming <benmunozj@gmail.com>
2023-12-27 13:54:56 -0500

    fix: console log typo

--- packages/be/liftoff.js
+++ packages/be/liftoff.js
@@ -70,11 +70,11 @@ const logDatabaseStatistics = async () => {
       const pockets = await MC.mongoInstances[instance].model.Pocket.find({})
       console.log(`${instance} total pockets: ${pockets.length}`)
       const portals = await MC.mongoInstances[instance].model.Portal.find({})
-      console.log(`${instance} total portals: ${portals.length}`)
+      console.log(`${instance} total prints: ${portals.length}`)
       const prints = await MC.mongoInstances[instance].model.Print.find({})
       console.log(`${instance} total portals: ${prints.length}`)
       const thingies = await MC.mongoInstances[instance].model.Thingie.find({})
-      console.log(`${instance} total portals: ${thingies.length}`)
+      console.log(`${instance} total thingies: ${thingies.length}`)
       console.log('===========================================================')
     }
   } catch (e) {
